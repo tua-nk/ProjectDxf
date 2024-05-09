ce04d8a662040beb9e2101d96f11ecae8b28ccb0
thaont-ulis <113043656+thaont-ulis@users.noreply.github.com>
2024-05-09 23:55:29 +0700

    update spline

--- dxflib++/include/entities/spline.h
+++ dxflib++/include/entities/spline.h
@@ -11,7 +11,18 @@ namespace dxflib::entities
 	{
 		double x0, y0, z0;
 		double x1, y1, z1;
-		double thickness;
+		double x2, y2, z2;
+		double x3, y3, z3;
+		double knot_value;
+		double weight_value;
+		double knot_tolerance;
+		double control_point_tolerance;
+		double fit_tolerance;
+		int flag;
+		int degree;
+		int number_of_knots;
+		int number_of_control_points;
+		int number_of_fit_points;
 
 		/**
 		 * \brief Parse function for spline buffer
@@ -32,13 +43,28 @@ namespace dxflib::entities
 		{
 			subclassmarker = 100,
 			error = -1,
-			thickness = 39,
 			x0 = 10,
 			x1 = 11,
+			x2 = 12,
+			x3 = 13,
 			y0 = 20,
 			y1 = 21,
+			y2 = 22,
+			y3 = 23,
 			z0 = 30,
 			z1 = 31,
+			z2 = 32,
+			z3 = 33,
+			knot_value = 40,
+			weight_value = 41,
+			knot_tolerance = 42,
+			control_point_tolerance = 43,
+			fit_tolerance = 44,
+			flag = 70,
+			degree = 71,
+			number_of_knots = 72,
+			number_of_control_points = 73,
+			number_of_fit_points = 74
 		};
 	}
 
@@ -54,14 +80,32 @@ namespace dxflib::entities
 
 		// public interface
 		const vertex& get_vertex(int id) const; // Returns the vertex given by it's id
-		void move_vertex(int id, const vertex& new_vertex); // sets the vertex given by id to new location
-		double get_thickness() const { return thickness_; } // Returns the thickness of the spline
-		double get_length() const { return length_; } // Returns the length of the spline
+		const double get_knot_value() const { return knot_value_;};
+		const double get_weight_value() const { return weight_value_; };
+		const double get_knot_tolerance() const { return knot_tolerance_; };
+		const double get_control_point_tolerance() const { return control_point_tolerance_; };
+		const double get_fit_tolerance() const { return fit_tolerance_; };
+		const double get_flag() const { return flag_; };
+		const double get_degree() const { return degree_; };
+		const double get_number_of_knots() const { return number_of_knots_; };
+		const double get_number_of_control_points() const { return number_of_control_points_; };
+		const double get_number_of_fit_points() const { return number_of_fit_points_; };
+
 	private:
 		// Properties
-		vertex v0_; // Starting Vertex
-		vertex v1_; // Ending Vertex
-		double thickness_; // Thickness of the spline
-		double length_; // distance from v0 to v1
+		vertex v0_;
+		vertex v1_;
+		vertex v2_;
+		vertex v3_;
+		double knot_value_;
+		double weight_value_;
+		double knot_tolerance_;
+		double control_point_tolerance_;
+		double fit_tolerance_;
+		int flag_;
+		int degree_;
+		int number_of_knots_;
+		int number_of_control_points_;
+		int number_of_fit_points_;
 	};
 }
--- dxflib++/src/entities/spline.cpp
+++ dxflib++/src/entities/spline.cpp
@@ -19,14 +19,17 @@ int dxflib::entities::spline_buffer::parse(const std::string& cl, const std::str
 	{
 	case spline_codes::error:
 		return 0;
-	case spline_codes::thickness:
-		thickness = std::stod(nl);
-		return 1;
 	case spline_codes::x0:
 		x0 = std::stod(nl);
 		return 1;
 	case spline_codes::x1:
 		x1 = std::stod(nl);
+		return 1;
+	case spline_codes::x2:
+		x2 = std::stod(nl);
+		return 1;
+	case spline_codes::x3:
+		x3 = std::stod(nl);
 		return 1;
 	case spline_codes::y0:
 		y0 = std::stod(nl);
@@ -34,11 +37,53 @@ int dxflib::entities::spline_buffer::parse(const std::string& cl, const std::str
 	case spline_codes::y1:
 		y1 = std::stod(nl);
 		return 1;
+	case spline_codes::y2:
+		y2 = std::stod(nl);
+		return 1;
+	case spline_codes::y3:
+		y3 = std::stod(nl);
+		return 1;
 	case spline_codes::z0:
 		z0 = std::stod(nl);
 		return 1;
 	case spline_codes::z1:
 		z1 = std::stod(nl);
+		return 1;
+	case spline_codes::z2:
+		z2 = std::stod(nl);
+		return 1;
+	case spline_codes::z3:
+		z3 = std::stod(nl);
+		return 1;
+	case spline_codes::knot_value:
+		knot_value = std::stod(nl);
+		return 1;
+	case spline_codes::weight_value:
+		weight_value = std::stod(nl);
+		return 1;
+	case spline_codes::knot_tolerance:
+		knot_tolerance = std::stod(nl);
+		return 1;
+	case spline_codes::control_point_tolerance:
+		control_point_tolerance = std::stod(nl);
+		return 1;
+	case spline_codes::fit_tolerance:
+		fit_tolerance = std::stod(nl);
+		return 1;
+	case spline_codes::flag:
+		flag = std::stod(nl);
+		return 1;
+	case spline_codes::degree:
+		degree = std::stod(nl);
+		return 1;
+	case spline_codes::number_of_knots:
+		number_of_knots = std::stod(nl);
+		return 1;
+	case spline_codes::number_of_control_points:
+		number_of_control_points = std::stod(nl);
+		return 1;
+	case spline_codes::number_of_fit_points:
+		number_of_fit_points = std::stod(nl);
 		return 1;
 	default:
 		return 0;
@@ -54,19 +99,44 @@ void dxflib::entities::spline_buffer::free()
 	x1 = 0;
 	y1 = 0;
 	z1 = 0;
-	thickness = 0;
+	x2 = 0;
+	y2 = 0;
+	z2 = 0;
+	x3 = 0;
+	y3 = 0;
+	z3 = 0;
+	knot_value = 0;
+	weight_value = 0;
+	knot_tolerance = 0;
+	control_point_tolerance = 0;
+	fit_tolerance = 0;
+	flag = 0;
+	degree = 0;
+	number_of_knots = 0;
+	number_of_control_points = 0;
+	number_of_fit_points = 0;
 }
 
 /**
  * \brief Spline buffer constructor for the spline entity
  * \param lb Spline Buffer
  */
-dxflib::entities::spline::spline(spline_buffer& lb) :
-	entity(lb),
-	v0_(lb.x0, lb.y0, lb.z0),
-	v1_(lb.x1, lb.y1, lb.z1),
-	thickness_(lb.thickness),
-	length_(mathlib::distance(v0_, v1_))
+dxflib::entities::spline::spline(spline_buffer& sb) :
+	entity(sb),
+	v0_(sb.x0, sb.y0, sb.z0),
+	v1_(sb.x1, sb.y1, sb.z1),
+	v2_(sb.x2, sb.y2, sb.z2),
+	v3_(sb.x3, sb.y3, sb.z3),
+	knot_value_(sb.knot_value),
+	weight_value_(sb.weight_value),
+	knot_tolerance_(sb.knot_tolerance),
+	control_point_tolerance_(sb.control_point_tolerance),
+	fit_tolerance_(sb.fit_tolerance),
+	flag_(sb.flag),
+	degree_(sb.degree),
+	number_of_knots_(sb.number_of_knots),
+	number_of_control_points_(sb.number_of_control_points),
+	number_of_fit_points_(sb.number_of_fit_points)
 {
 }
 
@@ -76,17 +146,8 @@ const dxflib::entities::vertex& dxflib::entities::spline::get_vertex(const int i
 	{
 	case 0: return v0_;
 	case 1: return v1_;
+	case 2: return v2_;
+	case 3: return v3_;
 	default: return v0_;
 	}
-}
-
-void dxflib::entities::spline::move_vertex(const int id, const vertex& new_vertex)
-{
-	switch (id)
-	{
-	case 0: v0_ = new_vertex;
-	case 1: v1_ = new_vertex;
-	default: ;
-	}
-	length_ = mathlib::distance(v0_, v1_);
 }
